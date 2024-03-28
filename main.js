9f00a8faa8b0510fa431434b9ae8109f6a969606
thiagonati <72274069+thiagonati@users.noreply.github.com>
2024-03-27 22:45:57 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.multiply = void 0;
+function multiply(a, b) {
+    return a * b;
+}
+exports.multiply = multiply;
+console.log(multiply(2, 5));
