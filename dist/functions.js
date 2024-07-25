70faf815d6343204ef710f1ec17b1219ce402ceb
thiagonati <72274069+thiagonati@users.noreply.github.com>
2024-07-25 20:57:56 -0300

    feat: functions js

--- dist/functions.js
+++ dist/functions.js
@@ -1,7 +1,23 @@
 "use strict";
 const add = (a, b) => a + b;
-console.log(add(2, 2));
+// console.log(add(2, 2));
 const addWithOptionalTax = function (a, b, tax) {
-    return a + b - tax;
+    if (typeof tax != "undefined") {
+        return a + b - tax;
+    }
+    return a + b;
 };
-console.log(addWithOptionalTax(2, 2, 1));
+// console.log(addWithOptionalTax(2, 2, 1));
+const applyDiscount = (prince, percentage = 5) => {
+    return prince - prince * (percentage / 100);
+};
+// console.log(applyDiscount(10000, 10));
+const calcTotal = (name, ...numbers) => {
+    let total = 0;
+    console.log(name);
+    numbers.forEach((num) => {
+        return (total += num);
+    });
+    return total;
+};
+console.log(calcTotal("Thiago", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
--- src/functions.ts
+++ src/functions.ts
@@ -1,12 +1,32 @@
 const add = (a: number, b: number): number => a + b;
-console.log(add(2, 2));
+// console.log(add(2, 2));
 
 const addWithOptionalTax = function (
   a: number,
   b: number,
-  tax: number
+  tax?: number
 ): number {
-  return a + b - tax;
+  if (typeof tax != "undefined") {
+    return a + b - tax;
+  }
+  return a + b;
 };
 
-console.log(addWithOptionalTax(2, 2, 1));
+// console.log(addWithOptionalTax(2, 2, 1));
+
+const applyDiscount = (prince: number, percentage: number = 5): number => {
+  return prince - prince * (percentage / 100);
+};
+
+// console.log(applyDiscount(10000, 10));
+
+const calcTotal = (name: string, ...numbers: number[]): number => {
+  let total = 0;
+  console.log(name);
+  numbers.forEach((num) => {
+    return (total += num);
+  });
+  return total;
+};
+
+console.log(calcTotal("Thiago", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
